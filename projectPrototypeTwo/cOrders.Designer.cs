275b0397100e2ba232f801fff0f7862b8727a1bc
RameshRukshan <ramesharukshan3@gmail.com>
2021-11-26 22:59:00 +0530

    Custom Order form added

---
+++ projectPrototypeTwo/cOrders.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace projectPrototypeTwo
+{
+    partial class cOrders
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(cOrders));
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2PictureBox2 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.img_WhiteRobot = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.img_BlueRobot = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.img_redRobo = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.img_YellowRobot = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.rad_white = new Guna.UI2.WinForms.Guna2ImageRadioButton();
+            this.rad_blue = new Guna.UI2.WinForms.Guna2ImageRadioButton();
+            this.rad_red = new Guna.UI2.WinForms.Guna2ImageRadioButton();
+            this.rad_yellow = new Guna.UI2.WinForms.Guna2ImageRadioButton();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_WhiteRobot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_BlueRobot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_redRobo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_YellowRobot)).BeginInit();
+            this.guna2CustomGradientPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Controls.Add(this.guna2HtmlLabel1);
+            this.guna2Panel1.Controls.Add(this.guna2PictureBox2);
+            this.guna2Panel1.Controls.Add(this.guna2ControlBox1);
+            this.guna2Panel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(124)))), ((int)(((byte)(124)))));
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
+            this.guna2Panel1.Size = new System.Drawing.Size(857, 36);
+            this.guna2Panel1.TabIndex = 1;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(52, 12);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(72, 15);
+            this.guna2HtmlLabel1.TabIndex = 2;
+            this.guna2HtmlLabel1.Text = "Custom Orders";
+            // 
+            // guna2PictureBox2
+            // 
+            this.guna2PictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox2.Image")));
+            this.guna2PictureBox2.ImageRotate = 0F;
+            this.guna2PictureBox2.Location = new System.Drawing.Point(11, 4);
+            this.guna2PictureBox2.Name = "guna2PictureBox2";
+            this.guna2PictureBox2.ShadowDecoration.Parent = this.guna2PictureBox2;
+            this.guna2PictureBox2.Size = new System.Drawing.Size(34, 30);
+            this.guna2PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox2.TabIndex = 1;
+            this.guna2PictureBox2.TabStop = false;
+            this.guna2PictureBox2.UseTransparentBackground = true;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+            this.guna2ControlBox1.HoverState.FillColor = System.Drawing.Color.Gray;
+            this.guna2ControlBox1.HoverState.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.guna2ControlBox1.HoverState.Parent = this.guna2ControlBox1;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(801, 0);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.ShadowDecoration.Parent = this.guna2ControlBox1;
+            this.guna2ControlBox1.Size = new System.Drawing.Size(55, 35);
+            this.guna2ControlBox1.TabIndex = 0;
+            // 
+            // img_WhiteRobot
+            // 
+            this.img_WhiteRobot.BackColor = System.Drawing.Color.Transparent;
+            this.img_WhiteRobot.Image = ((System.Drawing.Image)(resources.GetObject("img_WhiteRobot.Image")));
+            this.img_WhiteRobot.ImageRotate = 0F;
+            this.img_WhiteRobot.Location = new System.Drawing.Point(65, 82);
+            this.img_WhiteRobot.Name = "img_WhiteRobot";
+            this.img_WhiteRobot.ShadowDecoration.Parent = this.img_WhiteRobot;
+            this.img_WhiteRobot.Size = new System.Drawing.Size(275, 263);
+            this.img_WhiteRobot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.img_WhiteRobot.TabIndex = 2;
+            this.img_WhiteRobot.TabStop = false;
+            this.img_WhiteRobot.UseTransparentBackground = true;
+            // 
+            // img_BlueRobot
+            // 
+            this.img_BlueRobot.BackColor = System.Drawing.Color.Transparent;
+            this.img_BlueRobot.Image = ((System.Drawing.Image)(resources.GetObject("img_BlueRobot.Image")));
+            this.img_BlueRobot.ImageRotate = 0F;
+            this.img_BlueRobot.Location = new System.Drawing.Point(65, 82);
+            this.img_BlueRobot.Name = "img_BlueRobot";
+            this.img_BlueRobot.ShadowDecoration.Parent = this.img_BlueRobot;
+            this.img_BlueRobot.Size = new System.Drawing.Size(275, 263);
+            this.img_BlueRobot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.img_BlueRobot.TabIndex = 2;
+            this.img_BlueRobot.TabStop = false;
+            this.img_BlueRobot.UseTransparentBackground = true;
+            // 
+            // img_redRobo
+            // 
+            this.img_redRobo.BackColor = System.Drawing.Color.Transparent;
+            this.img_redRobo.Image = ((System.Drawing.Image)(resources.GetObject("img_redRobo.Image")));
+            this.img_redRobo.ImageRotate = 0F;
+            this.img_redRobo.Location = new System.Drawing.Point(65, 82);
+            this.img_redRobo.Name = "img_redRobo";
+            this.img_redRobo.ShadowDecoration.Parent = this.img_redRobo;
+            this.img_redRobo.Size = new System.Drawing.Size(275, 263);
+            this.img_redRobo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.img_redRobo.TabIndex = 2;
+            this.img_redRobo.TabStop = false;
+            this.img_redRobo.UseTransparentBackground = true;
+            // 
+            // img_YellowRobot
+            // 
+            this.img_YellowRobot.BackColor = System.Drawing.Color.Transparent;
+            this.img_YellowRobot.Image = ((System.Drawing.Image)(resources.GetObject("img_YellowRobot.Image")));
+            this.img_YellowRobot.ImageRotate = 0F;
+            this.img_YellowRobot.Location = new System.Drawing.Point(65, 82);
+            this.img_YellowRobot.Name = "img_YellowRobot";
+            this.img_YellowRobot.ShadowDecoration.Parent = this.img_YellowRobot;
+            this.img_YellowRobot.Size = new System.Drawing.Size(275, 263);
+            this.img_YellowRobot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.img_YellowRobot.TabIndex = 2;
+            this.img_YellowRobot.TabStop = false;
+            this.img_YellowRobot.UseTransparentBackground = true;
+            // 
+            // guna2CustomGradientPanel1
+            // 
+            this.guna2CustomGradientPanel1.Controls.Add(this.guna2Button1);
+            this.guna2CustomGradientPanel1.Controls.Add(this.rad_yellow);
+            this.guna2CustomGradientPanel1.Controls.Add(this.rad_red);
+            this.guna2CustomGradientPanel1.Controls.Add(this.rad_blue);
+            this.guna2CustomGradientPanel1.Controls.Add(this.rad_white);
+            this.guna2CustomGradientPanel1.Controls.Add(this.guna2HtmlLabel2);
+            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(471, 82);
+            this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            this.guna2CustomGradientPanel1.ShadowDecoration.Parent = this.guna2CustomGradientPanel1;
+            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(311, 353);
+            this.guna2CustomGradientPanel1.TabIndex = 3;
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(43, 14);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(72, 21);
+            this.guna2HtmlLabel2.TabIndex = 0;
+            this.guna2HtmlLabel2.Text = "COLORS";
+            // 
+            // rad_white
+            // 
+            this.rad_white.CheckedState.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image3")));
+            this.rad_white.CheckedState.ImageSize = new System.Drawing.Size(40, 40);
+            this.rad_white.CheckedState.Parent = this.rad_white;
+            this.rad_white.HoverState.Parent = this.rad_white;
+            this.rad_white.Image = ((System.Drawing.Image)(resources.GetObject("rad_white.Image")));
+            this.rad_white.ImageOffset = new System.Drawing.Point(0, 0);
+            this.rad_white.ImageRotate = 0F;
+            this.rad_white.ImageSize = new System.Drawing.Size(30, 30);
+            this.rad_white.Location = new System.Drawing.Point(34, 43);
+            this.rad_white.Name = "rad_white";
+            this.rad_white.PressedState.Parent = this.rad_white;
+            this.rad_white.ShadowDecoration.Parent = this.rad_white;
+            this.rad_white.Size = new System.Drawing.Size(41, 41);
+            this.rad_white.TabIndex = 1;
+            // 
+            // rad_blue
+            // 
+            this.rad_blue.CheckedState.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image2")));
+            this.rad_blue.CheckedState.ImageSize = new System.Drawing.Size(40, 40);
+            this.rad_blue.CheckedState.Parent = this.rad_blue;
+            this.rad_blue.HoverState.Parent = this.rad_blue;
+            this.rad_blue.Image = ((System.Drawing.Image)(resources.GetObject("rad_blue.Image")));
+            this.rad_blue.ImageOffset = new System.Drawing.Point(0, 0);
+            this.rad_blue.ImageRotate = 0F;
+            this.rad_blue.ImageSize = new System.Drawing.Size(30, 30);
+            this.rad_blue.Location = new System.Drawing.Point(81, 43);
+            this.rad_blue.Name = "rad_blue";
+            this.rad_blue.PressedState.Parent = this.rad_blue;
+            this.rad_blue.ShadowDecoration.Parent = this.rad_blue;
+            this.rad_blue.Size = new System.Drawing.Size(41, 41);
+            this.rad_blue.TabIndex = 1;
+            // 
+            // rad_red
+            // 
+            this.rad_red.CheckedState.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image1")));
+            this.rad_red.CheckedState.ImageSize = new System.Drawing.Size(40, 40);
+            this.rad_red.CheckedState.Parent = this.rad_red;
+            this.rad_red.HoverState.Parent = this.rad_red;
+            this.rad_red.Image = ((System.Drawing.Image)(resources.GetObject("rad_red.Image")));
+            this.rad_red.ImageOffset = new System.Drawing.Point(0, 0);
+            this.rad_red.ImageRotate = 0F;
+            this.rad_red.ImageSize = new System.Drawing.Size(30, 30);
+            this.rad_red.Location = new System.Drawing.Point(128, 43);
+            this.rad_red.Name = "rad_red";
+            this.rad_red.PressedState.Parent = this.rad_red;
+            this.rad_red.ShadowDecoration.Parent = this.rad_red;
+            this.rad_red.Size = new System.Drawing.Size(41, 41);
+            this.rad_red.TabIndex = 1;
+            // 
+            // rad_yellow
+            // 
+            this.rad_yellow.CheckedState.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image")));
+            this.rad_yellow.CheckedState.ImageSize = new System.Drawing.Size(40, 40);
+            this.rad_yellow.CheckedState.Parent = this.rad_yellow;
+            this.rad_yellow.HoverState.Parent = this.rad_yellow;
+            this.rad_yellow.Image = ((System.Drawing.Image)(resources.GetObject("rad_yellow.Image")));
+            this.rad_yellow.ImageOffset = new System.Drawing.Point(0, 0);
+            this.rad_yellow.ImageRotate = 0F;
+            this.rad_yellow.ImageSize = new System.Drawing.Size(30, 30);
+            this.rad_yellow.Location = new System.Drawing.Point(175, 43);
+            this.rad_yellow.Name = "rad_yellow";
+            this.rad_yellow.PressedState.Parent = this.rad_yellow;
+            this.rad_yellow.ShadowDecoration.Parent = this.rad_yellow;
+            this.rad_yellow.Size = new System.Drawing.Size(41, 41);
+            this.rad_yellow.TabIndex = 1;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.Animated = true;
+            this.guna2Button1.AutoRoundedCorners = true;
+            this.guna2Button1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button1.BorderRadius = 11;
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.DisabledState.Parent = this.guna2Button1;
+            this.guna2Button1.FillColor = System.Drawing.Color.Silver;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(44, 97);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(181, 24);
+            this.guna2Button1.TabIndex = 2;
+            this.guna2Button1.Text = "Apply Colors";
+            this.guna2Button1.UseTransparentBackground = true;
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // cOrders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(852, 475);
+            this.Controls.Add(this.guna2CustomGradientPanel1);
+            this.Controls.Add(this.img_redRobo);
+            this.Controls.Add(this.img_YellowRobot);
+            this.Controls.Add(this.img_BlueRobot);
+            this.Controls.Add(this.img_WhiteRobot);
+            this.Controls.Add(this.guna2Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "cOrders";
+            this.Text = "cOrders";
+            this.Load += new System.EventHandler(this.cOrders_Load);
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_WhiteRobot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_BlueRobot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_redRobo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_YellowRobot)).EndInit();
+            this.guna2CustomGradientPanel1.ResumeLayout(false);
+            this.guna2CustomGradientPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox2;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2PictureBox img_WhiteRobot;
+        private Guna.UI2.WinForms.Guna2PictureBox img_BlueRobot;
+        private Guna.UI2.WinForms.Guna2PictureBox img_redRobo;
+        private Guna.UI2.WinForms.Guna2PictureBox img_YellowRobot;
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
+        private Guna.UI2.WinForms.Guna2ImageRadioButton rad_yellow;
+        private Guna.UI2.WinForms.Guna2ImageRadioButton rad_red;
+        private Guna.UI2.WinForms.Guna2ImageRadioButton rad_blue;
+        private Guna.UI2.WinForms.Guna2ImageRadioButton rad_white;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+    }
+}
---
+++ projectPrototypeTwo/cOrders.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projectPrototypeTwo
+{
+    public partial class cOrders : Form
+    {
+        public cOrders()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void cOrders_Load(object sender, EventArgs e)
+        {
+            img_BlueRobot.Visible = false;
+            img_redRobo.Visible = false;
+            img_YellowRobot.Visible = false;
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            if (rad_blue.Checked)
+            {
+                img_WhiteRobot.Visible = false;
+                img_BlueRobot.Visible = true;
+                img_redRobo.Visible = false;
+                img_YellowRobot.Visible = false;
+            }else if (rad_red.Checked)
+            {
+                img_WhiteRobot.Visible = false;
+                img_BlueRobot.Visible = false;
+                img_redRobo.Visible = true;
+                img_YellowRobot.Visible = false;
+            }
+            else if (rad_yellow.Checked)
+            {
+                img_WhiteRobot.Visible = false;
+                img_BlueRobot.Visible = false;
+                img_redRobo.Visible = false;
+                img_YellowRobot.Visible = true;
+            }
+            else
+            {
+                img_WhiteRobot.Visible = true;
+                img_BlueRobot.Visible = false;
+                img_redRobo.Visible = false;
+                img_YellowRobot.Visible = false;
+            }
+        }
+    }
+}

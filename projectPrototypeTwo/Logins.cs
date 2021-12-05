ac4e7ef84e8e6e04e4434173a6e7ec64c564be1a
RameshRukshan <ramesharukshan3@gmail.com>
2021-12-05 19:14:28 +0530

    Set new calss to Control User logins.

---
+++ projectPrototypeTwo/Logins.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectPrototypeTwo
+{
+    class Logins
+    {
+        bool loginStatus = false;
+        string loggedUserName ;
+
+        public void checkLoginStatus()
+        {
+
+        }
+
+        public void updateLoginStatus(bool value)
+        {
+            loginStatus = value;
+        }
+    }
+}

5d2498b94770c1a80ad5cd7d7a927df604bb1dcd
RameshRukshan <ramesharukshan3@gmail.com>
2021-12-09 20:45:43 +0530

    Updated with Class to connect db

---
+++ projectPrototypeTwo/dbConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace projectPrototypeTwo
+{
+    class dbConnection
+    {
+        string connectionString = "Server=tcp:server-nibm-rar.database.windows.net,1433;Initial Catalog=dbMegaTech;Persist Security Info=False;User ID=ramesh_rukshan;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private SqlDataAdapter da;
+        private SqlDataReader dr;
+
+        public void setConnection()
+        {
+            con = new SqlConnection(connectionString);
+        }
+        public void openConnection()
+        {
+            con.Open();
+        }
+        public void closeConnection()
+        {
+            con.Close();
+        }
+        
+    }
+}
